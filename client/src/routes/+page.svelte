2aad94b77119d0af150c2aa98831f681ac979a56
Siddarth kotthur <sid@Siddarths-MacBook-Pro.local>
2024-11-03 01:05:49 -0600

    Barebones mosiac

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,26 +1,11 @@
 <script>
-// import Sidebar from '../lib/components/ui/sidebar/Sidebar.svelte';
-import Mosiac from '../lib/Mosiac.svelte';
+  // Import the `embeds` data from the `load` function
+  import Mosiac from '../lib/Mosiac.svelte';
 
-let props = $props();
-const recommendations = [
-      { link: 'https://www.streameast.gd/game/cfb/82148814', confidence: 0.4 },
-      { link: 'https://svelte.dev/docs/kit/load', confidence: 0.5 },
-      {
-        link: 'https://stackoverflow.com/questions/34600003/converting-json-to-string-in-python',
-        confidence: 0.1
-      },
-      {
-        link: 'https://stackoverflow.com/questions/34600003/converting-json-to-string-in-python',
-        confidence: 0.8
-      },
-      {
-        link: 'https://stackoverflow.com/questions/34600003/converting-json-to-string-in-python',
-        confidence: 0.7
-      }
-    ];
+  let { data } = $props();
+
+  let embeds = data.data
 </script>
 
-<div> 
-  <Mosiac {recommendations} />
-</div>
+<Mosiac recommendations={embeds} />
+
