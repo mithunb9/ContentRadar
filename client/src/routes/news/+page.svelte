1d9e5eaf8f6e24bc0bc3321292764f7cccc7635b
Mithun Balasubramanian <sendmithunb@gmail.com>
2024-11-03 06:31:02 -0600

    add routes

---
+++ client/src/routes/news/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    // Import the `embeds` data from the `load` function
+    import Mosiac from '../../lib/Mosiac.svelte';
+  
+    let { data } = $props();
+  
+    let embeds = data.data
+  </script>
+  
+  <div class="w-[100%]">
+    <Mosiac recommendations={embeds} />
+  </div>
+  
+  
