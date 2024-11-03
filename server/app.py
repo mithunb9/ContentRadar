22f916fcf048f703343b9d38591220f02a032c9e
Mithun Balasubramanian <sendmithunb@gmail.com>
2024-11-02 22:20:12 -0500

    get embed link

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from util import get_link_data
 
 app = Flask(__name__)
 
@@ -6,6 +7,15 @@ app = Flask(__name__)
 def accept_html():
     html = request.args.get('html')
     return html
+
+@app.route('/embed', methods=['GET'])
+def get_embed():
+    link = request.args.get('link')
+    if link:
+        info = get_link_data(link)
+        return info
+    else:
+        return "No link provided", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
