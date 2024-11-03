657c7079cbcc7ae4ae9904a4e72204016e11d73b
Siddarth kotthur <sid@Siddarths-MacBook-Pro.local>
2024-11-02 21:09:07 -0500

    added frontend support

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let props = $props();
+</script>
+
+<h1>{props.data.recommendations}</h1>
