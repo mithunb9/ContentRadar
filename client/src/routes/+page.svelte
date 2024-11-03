bbb696f5f5350539c06dc1178fbe2b758e81e679
Mithun Balasubramanian <sendmithunb@gmail.com>
2024-11-02 21:46:44 -0500

    create sidebar

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import Sidebar from '../lib/components/ui/sidebar/Sidebar.svelte';
+</script>
+
+<div> 
+  <Sidebar />
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
